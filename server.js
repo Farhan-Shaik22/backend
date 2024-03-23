ed1a13316edbbd99bbf4d12ec779619cafb6168e
hydroxy42 <farhan2262003@gmail.com>
2024-03-23 11:41:50 +0530

    finalig6

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const secretKey = 'secret';
+const secretKey = 'fa7b20520f8922f6c1ce97fc';
 
 
 app.use(bodyParser.json());
